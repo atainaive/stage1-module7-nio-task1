3f20bac42aa57f8aaea57eb8802c0925299430ba
atainaive <atai.omorov@stud.vgtu.lt>
2022-09-23 15:32:32 +0300

    done

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,33 @@
 package com.epam.mjc.nio;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        String[] content = new String[0];
+        try {
+            content = Files.readString(file.toPath()).split("\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Map<String, String> contentMap = new HashMap<>();
+        for (String s : content) {
+            String[] pair = s.split(":");
+            contentMap.put(pair[0], pair[1].trim());
+        }
+
+        return new Profile(
+                contentMap.get("Name"),
+                Integer.parseInt(contentMap.get("Age")),
+                contentMap.get("Email"),
+                Long.parseLong(contentMap.get("Phone"))
+        );
     }
 }
